ee05fb59176c71a100a38e06ca6f6209580b298c
Sebastian Schuhmann <info@kacecfox.com>
2020-10-12 13:51:37 +0200

    Working on T241
    Summary: Working on T241
    
    Test Plan: Adding multiple emails should work now.
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D375

--- bin/templates/redirection/create.php
+++ bin/templates/redirection/create.php
@@ -22,16 +22,16 @@
 							<div class="span l1">
 								<label>Since</label>
 								<select class="frm-ctrl" name="targets[0][since]">
-									<?php foreach (['Jetzt' => time(), 'In 20 Minuten' => time() + 1200] as $readable => $time): ?>
-									<option value="<?= $time ?>"><?= __($readable) ?></option>
+									<?php foreach (['Jetzt' => time(), 'In 20 Minuten' => time() + 1200] as $readable => $time) : ?>
+										<option value="<?= $time ?>"><?= __($readable) ?></option>
 									<?php endforeach; ?>
 								</select>
 							</div>
 							<div class="span l1">
 								<label>To</label>
 								<select class="frm-ctrl" name="targets[0][until]">
-									<?php foreach (['In 20 Minuten' => time() + 1200, 'In 1 Jahr' => time() + 86400 * 365] as $readable => $time): ?>
-									<option value="<?= $time ?>"><?= __($readable) ?></option>
+									<?php foreach (['In 20 Minuten' => time() + 1200, 'In 1 Jahr' => time() + 86400 * 365] as $readable => $time) : ?>
+										<option value="<?= $time ?>"><?= __($readable) ?></option>
 									<?php endforeach; ?>
 								</select>
 							</div>
@@ -47,19 +47,54 @@
 							<div class="span l1">
 								<label>Since</label>
 								<select class="frm-ctrl" name="targets[1][since]">
-									<?php foreach (['Jetzt' => time(), 'In 20 Minuten' => time() + 1200] as $readable => $time): ?>
-									<option value="<?= $time ?>"><?= __($readable) ?></option>
+									<?php foreach (['Jetzt' => time(), 'In 20 Minuten' => time() + 1200] as $readable => $time) : ?>
+										<option value="<?= $time ?>"><?= __($readable) ?></option>
 									<?php endforeach; ?>
 								</select>
 							</div>
 							<div class="span l1">
 								<label>To</label>
 								<select class="frm-ctrl" name="targets[1][until]">
-									<?php foreach (['In 20 Minuten' => time() + 1200, 'In 1 Jahr' => time() + 86400 * 365] as $readable => $time): ?>
-									<option value="<?= $time ?>"><?= __($readable) ?></option>
+									<?php foreach (['In 20 Minuten' => time() + 1200, 'In 1 Jahr' => time() + 86400 * 365] as $readable => $time) : ?>
+										<option value="<?= $time ?>"><?= __($readable) ?></option>
 									<?php endforeach; ?>
 								</select>
 							</div>
+						</div>
+
+						<template data-lysine-view="email">
+							<div>
+								<div class="spacer small"></div>
+
+								<div class="row l3">
+									<div class="span l1">
+										<label>Email</label>
+										<input type="text" class="frm-ctrl" data-lysine-name="targets[{{id}}][email]" placeholder="Email">
+									</div>
+									<div class="span l1">
+										<label>Since</label>
+										<select class="frm-ctrl" data-lysine-name="targets[{{id}}][since]">
+											<?php foreach (['Jetzt' => time(), 'In 20 Minuten' => time() + 1200] as $readable => $time) : ?>
+												<option value="<?= $time ?>"><?= __($readable) ?></option>
+											<?php endforeach; ?>
+										</select>
+									</div>
+									<div class="span l1">
+										<label>To</label>
+										<select class="frm-ctrl" data-lysine-name="targets[{{id}}][until]">
+											<?php foreach (['In 20 Minuten' => time() + 1200, 'In 1 Jahr' => time() + 86400 * 365] as $readable => $time) : ?>
+												<option value="<?= $time ?>"><?= __($readable) ?></option>
+											<?php endforeach; ?>
+										</select>
+									</div>
+								</div>
+							</div>
+						</template>
+
+						<div class="spacer small"></div>
+
+						<div class="align-center">
+							<a id="btn-add-email" class="button outline small button-color-grey-300">Add an Email</a>
 						</div>
 
 						<div class="spacer small"></div>
@@ -76,3 +111,17 @@
 
 	</div>
 </div>
+
+
+
+<script lang="text/javascript">
+	depend(['m3/core/lysine'], function(Lysine) {
+		let counter = 2;
+		document.getElementById('btn-add-email').addEventListener('click', function() {
+			let v = new Lysine.view('email');
+			v.setData({
+				id: counter++
+			});
+		});
+	});
+</script>
